6d8a5378474d2a02243d0e88e79729101a282499
Gerardo Rodrigues <gerardofilho56@gmail.com>
2021-11-12 12:57:33 -0300

    Entidade Payment, associacao um pra um

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
 import com.example.course.entities.OrderItem;
+import com.example.course.entities.Payment;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-21T05:42:10Z"), o2);
+		o2.setPayment(pay1);
+		
+		orderRepository.save(o2);
 	}
 }
